55aa9f7accc4df07b57d2bf98369771a5ab8f5aa
sangamjadhav9911 <sangamjadhav9911@gmail.com>
2025-05-30 12:06:26 +0530

    mvc is updated

--- Router/Products.js
+++ Router/Products.js
@@ -1,40 +1,43 @@
 let express=require('express')
-const { allproduct } = require('../controller/Products')
+const { allproduct, singleproduct, deleteproduct, updateproduct } = require('../controller/Products')
 let Products=require('../model/Products')
 let router=express.Router()
 
-// router.get('/',allproduct)
-router.get('/',async(req,res)=>{
-    let data = await Products.find()
-    res.json(data)
-    // res.json("hello world")
-})
+router.get('/',allproduct)
+// router.get('/',async(req,res)=>{
+//     let data = await Products.find()
+//     res.json(data)
+//     // res.json("hello world")
+// })
 //get student by ID
-router.get('/:id', async (req, res) => {
-  let id = req.params.id
+router.get('/:id',singleproduct)
+// router.get('/:id', async (req, res) => {
+//   let id = req.params.id
 
-  let data = await Products.find({id:+id})
-  res.json(data)
-})
+//   let data = await Products.find({id:+id})
+//   res.json(data)
+// })
 
 
 
 //delete student by ID
-router.delete('/:id', async (req, res) => {
-  let id = req.params.id
+router.delete('/:id',deleteproduct)
+// router.delete('/:id', async (req, res) => {
+//   let id = req.params.id
 
-  let data = await Products.deleteOne({id:+id})
-  res.json('data deleted')
-})
+//   let data = await Products.deleteOne({id:+id})
+//   res.json('data deleted')
+// })
 
 //update student by ID
-router.patch('/:id', async (req, res) => {
-  let id = req.params.id
-  let prod = req.body
-  console.log(prod)
-  let data = await Products.updateOne({id:+id},{$set:prod})
-  res.json('data updated')
-})
+router.patch('/:id',updateproduct)
+// router.patch('/:id', async (req, res) => {
+//   let id = req.params.id
+//   let prod = req.body
+//   console.log(prod)
+//   let data = await Products.updateOne({id:+id},{$set:prod})
+//   res.json('data updated')
+// })
 
 
 //update student by ID
--- controller/Products.js
+++ controller/Products.js
@@ -1,11 +1,35 @@
-let products = require('../model/Products.js')
+let Products = require('../model/Products.js')
 
 let allproduct=async(req,res)=>{
-    let data = await products.find()
+    let data = await Products.find()
 
     res.json(data)
     // console.log(data)
 }
+let singleproduct=async (req, res) => {
+  let id = req.params.id
+
+  let data = await Products.find({id:+id})
+  res.json(data)
+}
+let deleteproduct=async (req, res) => {
+  let id = req.params.id
+
+  let data = await Products.deleteOne({id:+id})
+  res.json('data deleted')
+}
+let updateproduct= async (req, res) => {
+  let id = req.params.id
+  let prod = req.body
+  console.log(prod)
+  let data = await Products.updateOne({id:+id},{$set:prod})
+  res.json('data updated')
+}
+
 module.exports={
     allproduct,
+singleproduct,
+deleteproduct,
+updateproduct
+
 }
